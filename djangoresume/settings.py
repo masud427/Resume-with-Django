102dab65c7bb297fafe731614115feb7d9cf90fa
Masud <ama.masud71@gmail.com>
2024-10-31 18:23:58 +0100

    did heroku stuff

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
